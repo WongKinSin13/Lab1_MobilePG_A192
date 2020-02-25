6b9f9f7edc3004788b751c419ae5ca8877b966eb
WongKinSin <wongkinsin@ymail.com>
2020-02-25 18:21:09 +0800

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/lab1_fraction_calculator_253123/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lab1_fraction_calculator_253123
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
